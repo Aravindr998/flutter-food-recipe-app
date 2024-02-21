8a33a43cc014cc72026dc1f99bc1d91594428333
Aravind R <aravindr998@gmail.com>
2024-02-21 14:45:33 +0300

    Fix remove usematerial3 arg from themedata

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:meals/screens/tabs.dart';
 
 final theme = ThemeData(
-  useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
     seedColor: const Color.fromARGB(255, 131, 57, 0),
